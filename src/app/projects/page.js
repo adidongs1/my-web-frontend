a02c4b662ff9753caf7320eaf057b8180ad3a491
adidongs1 <adidongs15@gmail.com>
2025-09-07 23:27:39 +0700

    typo

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,13 +4,18 @@ import { useEffect, useState } from "react";
 
 export default function Projects() {
   const [data, setData] = useState(null);
-  const [show, setShow] = useState(false);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/hello`)
-      .then((res) => res.json())
-      .then((data) => setData(data));
-    setShow(true);
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`)
+      .then((res) => {
+        if (!res.ok) throw new Error("Network response was not ok");
+        return res.json();
+      })
+      .then((data) => setData(data))
+      .catch((err) => {
+        console.error("Fetch error:", err);
+        setData({ error: err.message });
+      });
   }, []);
 
   return <main className="min-h-screen flex flex-col gap-4 pt-8 px-16 text-white">{data && <pre>{JSON.stringify(data, null, 2)}</pre>}</main>;
